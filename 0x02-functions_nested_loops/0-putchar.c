a8eef8cecca9ec4c7327108918ef6c356f28a659
Traky <trakyrichardkanm3@gmail.com>
2022-03-16 04:09:29 +0000

    All c nested loop projects

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
-/*
-* main - determine if number is positive or negative
-* Description: program will assign a random number to the variable
-*
-* Return: Always 0 (success)
-*/
+/**
+ * main - check the code for ALX School students.
+ *
+ * Program that prints _putchar, followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
 
 	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This is the c loop functions
+This readme file describes functions and nested loop in C
