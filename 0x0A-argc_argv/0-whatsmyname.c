f06a84bf4617205d7f739eea03ea11c23b39a466
Traky <trakyrichardkanm3@gmail.com>
2022-04-02 08:49:46 +0000

    Add attribute unsed var

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print program name followed by a new line
-* without to compile it again
-*/
-int main(int __attribute__((__unused__)) argc, char *argv[])
+ * main - print program name followed by a new line
+ * without to compile it again
+ */
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-  return (0);
+	return (0);
 }
