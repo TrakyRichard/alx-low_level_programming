59452be02945afb8d5f83eb8d31203221e15be9e
Traky Richard <trakyrichardkanm3@gmail.com>
2022-06-12 10:30:09 +0000

    correct lint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h) {
+size_t print_dlistint(const dlistint_t *h)
+{
   size_t i = 0;
-  while (h != NULL) {
+  while (h != NULL)
+  {
     printf("%d\n", h->n);
     h = h->next;
     i++;
