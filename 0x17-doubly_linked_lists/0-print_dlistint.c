224d03507470a71e2aa4109db0e60a1866f32105
Traky Richard <trakyrichardkanm3@gmail.com>
2022-06-12 10:59:53 +0000

    correct lint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,14 +9,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t i = 0;
-  const dlistint_t *tmp = h;
+	size_t i = 0;
+	const dlistint_t *tmp = h;
 
-  while (tmp != NULL)
-  {
-    printf("%d\n", tmp->n);
-    tmp = tmp->next;
-    i++;
-  }
-  return (i);
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
 }
